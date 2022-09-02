9ad2cb0381469d12638607382a4919836cfd715c
kokanevina <kokane.vina@gmail.com>
2022-09-02 17:20:40 +0530

    ConfigContext

--- ServletInitPhase/src/main/java/com/servlets/CompanyServlet.java
+++ ServletInitPhase/src/main/java/com/servlets/CompanyServlet.java
@@ -3,6 +3,7 @@ package com.servlets;
 import java.io.IOException;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,7 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 /*if no annotations : web.xml : deployment descriptor : servlet, jsp, filters, listeners*/
 public class CompanyServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-   
+	String companyNm;
     public CompanyServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -24,14 +25,20 @@ public class CompanyServlet extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
     	// init parameters of servlets can be get using ServletConfig object
-    	String companyNm=config.getInitParameter("companyName");
+    	 companyNm=config.getInitParameter("companyName");
     	System.out.println(companyNm);
     	String address=config.getInitParameter("headOffice");
     	System.out.println(address);
+    	
+    	ServletContext context=config.getServletContext();
+    	String city=context.getInitParameter("city");
+    	System.out.println("Shared Data: "+city);
     }
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("in doGet");
+		System.out.println(companyNm);
+		
 	}
 
 	/**
@@ -41,4 +48,8 @@ public class CompanyServlet extends HttpServlet {
 		System.out.println("in doPost");
 	}
 
+	@Override
+	public void destroy() {
+		System.out.println(companyNm);
+	}
 }
